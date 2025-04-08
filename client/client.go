327d97549afed6632d0d0b1cc3844e0ced212a73
Erick Carrasco <erickype@hotmail.com>
2025-04-08 18:36:37 -0500

    Refactor: Add internal logger use

--- client/client.go
+++ client/client.go
@@ -3,12 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
+	"github.com/Erickype/DungeonPit/internal/log"
 	pb "github.com/Erickype/DungeonPit/proto"
 	"google.golang.org/grpc"
 )
@@ -16,46 +11,35 @@ import (
 func main() {
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("Could not connect: %v", err)
+		log.GetClientInstance().Fatal("Could not connect: %v", err)
 	}
-	defer conn.Close()
+	defer func(conn *grpc.ClientConn) {
+		err = conn.Close()
+		if err != nil {
+			log.GetClientInstance().Fatal("Could not close connection: %v", err)
+		}
+	}(conn)
 
 	client := pb.NewDungeonServiceClient(conn)
 
 	// 1. Login
 	var username string
 	fmt.Print("Enter username: ")
-	fmt.Scan(&username)
+	_, err = fmt.Scan(&username)
+	if err != nil {
+		log.GetClientInstance().Fatal("Could not read username:", err)
+	}
 	loginResp, err := client.Login(context.Background(), &pb.LoginRequest{Username: username})
 	if err != nil {
-		log.Fatalf("Login failed: %v", err)
+		log.GetClientInstance().Fatal("Login failed:", err)
 	}
-	log.Printf("Logged in as %s", loginResp.GetPlayerId())
+	log.GetClientInstance().Info("Logged in as:", loginResp.GetPlayerId())
 	playerID := loginResp.GetPlayerId()
-
-	// Logout
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		<-sigChan
-		log.Println("Caught interrupt signal, logging out...")
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-		defer cancel()
-
-		_, err := client.Logout(ctx, &pb.LogoutRequest{PlayerId: playerID})
-		if err != nil {
-			log.Printf("Logout failed: %v", err)
-		} else {
-			log.Println("Logged out successfully.")
-		}
-		os.Exit(0)
-	}()
 
 	// 2. GetCurrentRoom
 	roomResp, err := client.GetCurrentRoom(context.Background(), &pb.PlayerRequest{PlayerId: playerID})
 	if err != nil {
-		log.Fatalf("GetCurrentRoom failed: %v", err)
+		log.GetClientInstance().Fatal("GetCurrentRoom failed:", err)
 	}
 	logRoom(roomResp)
 
@@ -63,20 +47,35 @@ func main() {
 	for {
 		var moveDirection pb.Direction
 		fmt.Print("Enter move direction: ")
-		fmt.Scan(&moveDirection)
+		_, err = fmt.Scan(&moveDirection)
+		if err != nil {
+			log.GetClientInstance().Fatal("Could not read move direction:", err)
+		}
+
+		if moveDirection == pb.Direction_UNKNOWN {
+			log.GetClientInstance().Warn("Invalid move direction, logging out...")
+			break
+		}
 
 		moveResp, err := client.Move(context.Background(), &pb.MoveRequest{
 			PlayerId:  playerID,
 			Direction: moveDirection,
 		})
 		if err != nil {
-			log.Fatalf("Move failed: %v", err)
+			log.GetClientInstance().Fatal("Move failed:", err)
 		}
 		logRoom(moveResp)
 	}
 
+	// 4. Logout
+	_, err = client.Logout(context.Background(), &pb.LogoutRequest{PlayerId: playerID})
+	if err != nil {
+		log.GetClientInstance().Trace("Logout failed:", err)
+	} else {
+		log.GetClientInstance().Info("Logged out successfully.")
+	}
 }
 
 func logRoom(room *pb.RoomResponse) {
-	log.Printf("Room ID: %s", room.GetRoomId())
+	log.GetClientInstance().Info("Room ID:", room.GetRoomId())
 }
