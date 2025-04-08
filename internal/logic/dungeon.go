8d5826df15bd1845a6f4bf433cb1de4f3389e59d
Erick Carrasco <erickype@hotmail.com>
2025-04-08 18:36:12 -0500

    Refactor: Add internal logger use

--- internal/logic/dungeon.go
+++ internal/logic/dungeon.go
@@ -1,19 +1,20 @@
 package logic
 
 import (
+	"fmt"
+	"github.com/Erickype/DungeonPit/internal/log"
 	"github.com/Erickype/DungeonPit/internal/model"
-	"log"
 )
 
 func LoginPlayer(username string) (*model.Player, error) {
-	log.Printf("Login requested for username: %s", username)
+	log.GetCoreInstance().Info("Login requested for username:", username)
 	player := &model.Player{}
 	player.Username = username
 	err := player.Login()
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("Player %s succesfully loged in!", player.Username)
+	log.GetCoreInstance().Info("Player", player.Username, "succesfully logged in!")
 
 	return player, nil
 }
@@ -23,14 +24,15 @@ func SetInitialPlayerRoom(player *model.Player) (room *model.Room, err error) {
 	currentRoom := &model.Room{}
 	err = currentRoom.GetByRoomID(player.CurrentRoomID)
 	if err != nil {
-		log.Println("Player current room not found, teleporting to Room Zero")
+		log.GetCoreInstance().Info("Player current room not found, teleporting to Room Zero")
 		err = initialRoom.GetZeroRoom()
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		log.Printf("Player current room found, teleporting to Room (%d,%d,%d)",
+		message := fmt.Sprintf("Player current room found, teleporting to Room (%d, %d, %d)",
 			currentRoom.X, currentRoom.Y, currentRoom.Z)
+		log.GetCoreInstance().Info(message)
 		initialRoom = currentRoom
 	}
 
