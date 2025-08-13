5f8ff91e1b2750234bada3313a66fe29694efe06
Erick Carrasco <erickype@hotmail.com>
2025-08-12 19:19:39 -0500

    Feature: Add CalculateVertexDistance in Line2D

--- internal/core/DungeonGenerator2D/line2D.go
+++ internal/core/DungeonGenerator2D/line2D.go
@@ -1,14 +1,27 @@
 package core
 
-import "github.com/goki/mat32"
+import (
+	"github.com/goki/mat32"
+	"math"
+)
 
 type ILine2D interface {
 	IsSameLine2D(testLine Line2D) bool
+	CalculateVertexDistance() float64
 }
 
 type Line2D struct {
 	A mat32.Vec3
 	B mat32.Vec3
+}
+
+func (l *Line2D) CalculateVertexDistance() float64 {
+	aX := float64(l.A.X)
+	aY := float64(l.A.Y)
+	bX := float64(l.B.X)
+	bY := float64(l.B.Y)
+
+	return math.Sqrt(math.Pow(bX-aX, 2) + math.Pow(bY-aY, 2))
 }
 
 func (l *Line2D) IsSameLine2D(testLine Line2D) bool {
