226b198f18bcf0081924a15d0e81f40ec11ec4ce
Erick Carrasco <erickype@hotmail.com>
2025-08-21 13:39:23 -0500

    Feature: Pass Grid to data renderer

--- internal/core/DungeonGenerator2D/dataRenderer2D.go
+++ internal/core/DungeonGenerator2D/dataRenderer2D.go
@@ -1,6 +1,12 @@
 package core
 
-import "github.com/goki/mat32"
+import (
+	"math"
+	"slices"
+
+	"github.com/Erickype/DungeonPit/internal/core"
+	"github.com/goki/mat32"
+)
 
 type MoveDirection int
 
@@ -23,6 +29,7 @@ type IDataRenderer2D interface {
 }
 
 type DataRenderer2D struct {
+	Grid      map[mat32.Vec2]core.CellType
 	Rooms     []Room2D
 	Hallways  [][]mat32.Vec2
 	GridLines []GridLine
@@ -41,8 +48,26 @@ func (d *DataRenderer2D) CalculateHallways() {
 }
 
 func (d *DataRenderer2D) CalculateRooms() {
-	//TODO implement me
-	panic("implement me")
+	for i, hallway := range d.Hallways {
+		mstItem := d.MSTEdges[i]
+		vi := mat32.NewVec2(float32(math.Floor(float64(mstItem.A.X))), float32(math.Floor(float64(mstItem.A.Y))))
+		vf := hallway[0]
+		previousDirections := d.TwoVertexDirection(vi, vf)
+		slices.Insert(hallway, 0, vi)
+		for j := 0; j < len(hallway)-2; j++ {
+			vi = hallway[j]
+			vf = hallway[j+1]
+			currentDirections := d.TwoVertexDirection(vi, vf)
+			switch d.Grid[vi] {
+			case core.CellTypeRoom:
+				break
+			case core.CellTypeHallway:
+				break
+			case core.CellTypeNone:
+				break
+			}
+		}
+	}
 }
 
 func (d *DataRenderer2D) TwoVertexDirection(vi, vf mat32.Vec2) MoveDirection {
@@ -76,8 +101,9 @@ func (d *DataRenderer2D) PlaceHollowHallway() {
 	panic("implement me")
 }
 
-func NewDataRenderer2D(rooms []Room2D, hallways [][]mat32.Vec2, MSTEdges []Line2D) *DataRenderer2D {
+func NewDataRenderer2D(grid map[mat32.Vec2]core.CellType, rooms []Room2D, hallways [][]mat32.Vec2, MSTEdges []Line2D) *DataRenderer2D {
 	return &DataRenderer2D{
+		Grid:     grid,
 		Rooms:    rooms,
 		Hallways: hallways,
 		MSTEdges: MSTEdges,
--- internal/core/DungeonGenerator2D/generator2D.go
+++ internal/core/DungeonGenerator2D/generator2D.go
@@ -37,7 +37,7 @@ type DungeonGenerator2DSectionData struct {
 }
 
 func (d *DungeonGenerator2DSectionData) GenerateGridRenderData() {
-	d.DataRenderer = NewDataRenderer2D(d.Rooms, d.Hallways, d.MinimumSpanningTree2D.MSTEdges)
+	d.DataRenderer = NewDataRenderer2D(d.Grid, d.Rooms, d.Hallways, d.MinimumSpanningTree2D.MSTEdges)
 	d.DataRenderer.Calculate()
 }
 
