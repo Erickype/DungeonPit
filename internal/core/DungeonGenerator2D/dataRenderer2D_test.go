62fef81c0d42ba3717896d407cf5e17996153b12
Erick Carrasco <erickype@hotmail.com>
2025-08-28 15:28:35 -0500

    Test: add test data

--- internal/core/DungeonGenerator2D/dataRenderer2D_test.go
+++ internal/core/DungeonGenerator2D/dataRenderer2D_test.go
@@ -8,9 +8,10 @@ import (
 
 func TestDataRenderer2D_CalculateHallways(t *testing.T) {
 	rooms := []Room2D{
-		{Position: mat32.Vec2{X: 0, Y: 0}, Size: mat32.Vec2{X: 1, Y: 1}},
-		{Position: mat32.Vec2{X: 2, Y: 0}, Size: mat32.Vec2{X: 1, Y: 1}},
-		{Position: mat32.Vec2{X: 1, Y: 2}, Size: mat32.Vec2{X: 2, Y: 2}},
+		{Position: mat32.Vec2{X: 3, Y: 0}, Size: mat32.Vec2{X: 1, Y: 1}},
+		{Position: mat32.Vec2{X: 3, Y: 2}, Size: mat32.Vec2{X: 1, Y: 1}},
+		{Position: mat32.Vec2{X: 0, Y: 1}, Size: mat32.Vec2{X: 1, Y: 1}},
+		{Position: mat32.Vec2{X: 0, Y: 3}, Size: mat32.Vec2{X: 1, Y: 1}},
 	}
 	dungeon := DungeonGenerator2DSectionData{
 		Size:  mat32.Vec2{X: 5, Y: 5},
