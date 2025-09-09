9b713c8af0c6ffb374ac3e43c1d8166e081e5d37
Erick Carrasco <erickype@hotmail.com>
2025-09-09 16:17:43 -0500

    Feature: add gridline position enum for rendering details

--- internal/core/DungeonGenerator2D/gridLine2D.go
+++ internal/core/DungeonGenerator2D/gridLine2D.go
@@ -1,5 +1,16 @@
 package core
 
+type GridLinePosition int
+
+const (
+	GridLinePositionTop GridLinePosition = iota
+	GridLinePositionBottom
+	GridLinePositionLeft
+	GridLinePositionRight
+	GridLinePositionDoor
+	GridLinePositionHallwayPath
+)
+
 type GridLineType int
 
 const (
@@ -10,11 +21,19 @@ const (
 )
 
 type GridLine struct {
-	Line     Line2D
-	LineType GridLineType
+	Line         Line2D
+	LineType     GridLineType
+	LinePosition GridLinePosition
 }
 
-func NewGridLine(line Line2D, lineType GridLineType) *GridLine {
+func NewGridLine(line Line2D, lineType GridLineType, linePosition *GridLinePosition) *GridLine {
+	if linePosition != nil {
+		return &GridLine{
+			Line:         line,
+			LineType:     lineType,
+			LinePosition: *linePosition,
+		}
+	}
 	return &GridLine{
 		Line:     line,
 		LineType: lineType,
